beaf2fa8f5dca4dfc892d2dd81421d7161ff9ed9
Christopher Forte <80968869+cnf2500@users.noreply.github.com>
2023-10-09 14:27:15 -0500

    Add files via upload

---
+++ Projects/Dynamic Weather & Solar Positioning System/FirstLaurel.Build.cs
@@ -0,0 +1,37 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class FirstLaurel : ModuleRules
+{
+    public FirstLaurel(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        string ThirdPartyPath = Path.Combine(ModuleDirectory, "../../ThirdParty/");
+
+        // Add libcurl and OpenSSL include paths
+        PublicIncludePaths.AddRange(
+            new string[] {
+                Path.Combine(ThirdPartyPath, "libcurl", "7.83.1", "include"),
+                Path.Combine(ThirdPartyPath, "OpenSSL", "1.1.1n", "include", "Mac", "openssl")
+            }
+        );
+
+        // Add libraries
+        PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "libcurl", "7.83.1", "lib", "Mac", "Release", "libcurl.a"));
+        PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "OpenSSL", "1.1.1n", "lib", "Mac", "libcrypto.a"));
+        PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "OpenSSL", "1.1.1n", "lib", "Mac", "libssl.a"));
+        PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "nghttp2", "1.47.0", "lib", "Mac", "Release", "libnghttp2.a"));
+
+        // Link with SystemConfiguration and CoreLocation frameworks for macOS
+        if (Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            PublicFrameworks.AddRange(new string[] { "SystemConfiguration", "CoreLocation" });
+        }
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput", "Json", "JsonUtilities" });
+    }
+}
+
---
+++ Projects/Dynamic Weather & Solar Positioning System/Private/WeatherApi.cpp
@@ -0,0 +1,51 @@
+#include "WeatherApi.h"
+#include "Json.h"
+#include "JsonUtilities.h"
+#include <curl/curl.h>
+#include <sstream>
+
+WeatherApi::WeatherApi(const std::string& apiKey) : apiKey(apiKey), mode("json"), units("standard") {}
+
+size_t WeatherApi::WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string WeatherApi::makeRequest(const std::string& url) {
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+    curl = curl_easy_init();
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+    return readBuffer;
+}
+
+std::string WeatherApi::getByGeoLocation(double latitude, double longitude) {
+    std::stringstream url;
+    url << "https://api.openweathermap.org/data/2.5/weather?lat=" << latitude << "&lon=" << longitude << "&appid=" << apiKey;
+    return makeRequest(url.str());
+}
+
+void WeatherApi::setOutputMode(const std::string& newMode) {
+    this->mode = newMode;
+}
+
+void WeatherApi::setUnits(const std::string& newUnits) {
+    this->units = newUnits;
+}
+
+int32 WeatherApi::GetTimeZoneOffset(const std::string& jsonResponse) {
+    TSharedPtr<FJsonObject> JsonObject;
+    TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(jsonResponse.c_str());
+
+    if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid()) {
+        if(JsonObject->HasField("timezone")) {
+            return JsonObject->GetNumberField("timezone");
+        }
+    }
+    return 0; // Default to 0 if parsing fails
+}
